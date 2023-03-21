b7f0ef9bcd5147488b501544aefb1346c16d097f
unknown <watermelong518@gmail.com>
2023-03-21 10:38:51 +0700

    ok

---
+++ src/main/java/com/group4/applicationdevelopment/ApplicationDevelopment.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.group4.applicationdevelopment;
+
+/**
+ *
+ * @author ACER
+ */
+
+
+public class ApplicationDevelopment {
+
+    public static void main(String[] args) {
+        Option option = new Option();
+        option.setVisible(true);
+    }
+}
